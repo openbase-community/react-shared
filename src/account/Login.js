e3a683249080d94676104e43e42477d7acd6b024
Gabriel Montague <montaguegabe@users.noreply.github.com>
2025-09-09 13:47:29 -0400

    Revert "Revert login changes"
    This reverts commit e1d48dbae3094ca3d83dab59ff15e5089224171d.

--- src/account/Login.js
+++ src/account/Login.js
@@ -1,30 +1,15 @@
-import { useState } from 'react'
 import FormErrors from '../components/FormErrors'
-import { login } from '../lib/allauth'
 import { Link } from 'react-router-dom'
 import { useConfig } from '../auth'
 import ProviderList from '../socialaccount/ProviderList'
 import Button from '../components/Button'
 import WebAuthnLoginButton from '../mfa/WebAuthnLoginButton'
+import useLogin from '../hooks/useLogin'
 
 export default function Login () {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [response, setResponse] = useState({ fetching: false, content: null })
+  const { email, setEmail, password, setPassword, handleSubmit, isLoading, errors } = useLogin()
   const config = useConfig()
   const hasProviders = config.data.socialaccount?.providers?.length > 0
-
-  function submit () {
-    setResponse({ ...response, fetching: true })
-    login({ email, password }).then((content) => {
-      setResponse((r) => { return { ...r, content } })
-    }).catch((e) => {
-      console.error(e)
-      window.alert(e)
-    }).then(() => {
-      setResponse((r) => { return { ...r, fetching: false } })
-    })
-  }
   return (
     <div>
       <h1>Login</h1>
@@ -32,16 +17,16 @@ export default function Login () {
         No account? <Link to='/account/signup'>Sign up here.</Link>
       </p>
 
-      <FormErrors errors={response.content?.errors} />
+      <FormErrors errors={errors} />
 
       <div><label>Email <input value={email} onChange={(e) => setEmail(e.target.value)} type='email' required /></label>
-        <FormErrors param='email' errors={response.content?.errors} />
+        <FormErrors param='email' errors={errors} />
       </div>
       <div><label>Password: <input value={password} onChange={(e) => setPassword(e.target.value)} type='password' required /></label>
         <Link to='/account/password/reset'>Forgot your password?</Link>
-        <FormErrors param='password' errors={response.content?.errors} />
+        <FormErrors param='password' errors={errors} />
       </div>
-      <Button disabled={response.fetching} onClick={() => submit()}>Login</Button>
+      <Button disabled={isLoading} onClick={handleSubmit}>Login</Button>
       {config.data.account.login_by_code_enabled
         ? <Link className='btn btn-secondary' to='/account/login/code'>Send me a sign-in code</Link>
         : null}
